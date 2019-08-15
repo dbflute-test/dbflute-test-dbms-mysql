c24503cbbc301af9a2a392c954f586bf6de461c4
jflute <dbflute@gmail.com>
2019-08-15 22:08:34 +0900

    fix checkUser arguments

--- dbflute_resortlinedb/playsql/replace-schema.sql
+++ dbflute_resortlinedb/playsql/replace-schema.sql
@@ -4,9 +4,12 @@
 create database /*$dfprop.mainCatalog*/;
 
 -- #df:reviveUser()#
--- #df:checkUser(mainUser, grant)#
-grant all privileges on /*$dfprop.mainCatalog*/.*
-  to /*$dfprop.mainUser*/@localhost identified by '/*$dfprop.mainPassword*/';
+-- #df:checkUser(mainUser)#
+create user /*$dfprop.mainUser*/@localhost identified by '/*$dfprop.mainPassword*/';
+
+-- #df:reviveUser()#
+-- #df:checkUser(grant)#
+grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@localhost;
 
 flush privileges;
 
