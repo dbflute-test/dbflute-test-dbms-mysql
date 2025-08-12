4e95ef62a0b06f0198fc4b5536e8d9ca2783bb64
jflute <dbflute@gmail.com>
2025-08-12 17:31:56 +0900

    for MySQL-5.7 trial

--- dbflute_maihamadb/playsql/nextsql/replace-schema-00-system.sql
+++ dbflute_maihamadb/playsql/nextsql/replace-schema-00-system.sql
@@ -2,12 +2,12 @@
 -- #df:checkUser()#
 create database /*$dfprop.mainCatalog*/;
 
--- #df:reviveUser()#
--- #df:checkUser(mainUser)#
-create user /*$dfprop.mainUser*/@"%" identified by '/*$dfprop.mainPassword*/';
+-- #MySQL_5_7 cannot use create user statement
+-- so use "grant all privileges + identified by" here
 
 -- #df:reviveUser()#
 -- #df:checkUser(mainUser, grant)#
-grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@"%";
+grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@"%"
+    identified by '/*$dfprop.mainPassword*/';
 
 flush privileges;
--- dbflute_maihamadb/playsql/replace-schema-00-system.sql
+++ dbflute_maihamadb/playsql/replace-schema-00-system.sql
@@ -4,16 +4,17 @@
 -- #df:checkUser(mainSchema)#
 create database /*$dfprop.mainCatalog*/;
 
--- #df:reviveUser()#
--- #df:checkUser(mainUser)#
-create user /*$dfprop.mainUser*/@"%" identified by '/*$dfprop.mainPassword*/';
+-- #MySQL_5_7 cannot use create user statement
+-- so use "grant all privileges + identified by" here
 
 -- #df:reviveUser()#
 -- #df:checkUser(grant)#
-grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@"%";
+grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@"%"
+    identified by '/*$dfprop.mainPassword*/';
 
 -- #df:reviveUser()#
 -- #df:checkUser(grant)#
-grant all privileges on /*$nextCatalog*/.* to /*$dfprop.mainUser*/@"%";
+grant all privileges on /*$nextCatalog*/.* to /*$dfprop.mainUser*/@"%"
+    identified by '/*$dfprop.mainPassword*/';
 
 flush privileges;
